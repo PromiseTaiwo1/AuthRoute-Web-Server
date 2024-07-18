68530481dd32fe96cab11dbc0375fa62efec485a
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-07-18 16:48:48 +0100

    RESET

--- log_functions/logging.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOGGING_H
-#define LOGGING_H
-
-void log_stdout(int count, ...);
-void log_error(int count, ...);
-void log_file(int count, char *msg, ...);
-
-#endif
---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by Dada Clement Authori on 7/18/2024.
+//
+
+#include "main.h"
